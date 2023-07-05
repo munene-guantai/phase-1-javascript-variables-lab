dccff4b51d8cc42bf202eda64c66d52ac2449245
Mikebrian <mikemunene1173@gmail.com>
2023-07-05 15:10:03 +0300

    Complete assignment: Javascript-Variables-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+
+function example() {
+    const companyName = 'Immutable Company';
+    console.log(companyName)
+} 
+console.log(companyName)
+
+let mostProfitableNeighborhood = "Chelsea"// Code your solution in this file!
+
+let companyCeo = "Susan Smith"
